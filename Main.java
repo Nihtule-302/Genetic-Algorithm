dfe841d4894522abb3b15930705ed3516f6d0547
Ali wafa <30670268+Nihtule-302@users.noreply.github.com>
2023-05-29 16:38:17 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 public class Main {
+
     public static double[] evaluate(double[] generation){
         double[] evaluation = new double[generation.length];
         for (int i = 0; i < generation.length; i++) {
@@ -6,18 +7,38 @@ public class Main {
         }
         return evaluation;
     }
-    public static void select(double[] generation, double[] evaluation){}
-    public static void crossOver(){}
-    public static void mutate(){}
 
-    public static void Replacment(double[] generation, double[] evaluation){
-        select(generation, evaluation);
-        crossOver();
-        mutate();
+    public static double[] select(double[] generation, double[] evaluation){
+        double[] parents = new double[5];
+        double min = generation[0];
+        for (int i = 0; i < parents.length; i++) {
+            for (int j = 0; j < parents.length; j++) {
+                if (min > generation[j]){
+                    min = generation[j];
+                    generation[j] = 10000 // large number to avoid checking it again
+                }
+            }
+            parents[i] = min;
+        }
+        return parents
+    }
+
+    public static void crossOver(double[] parents){}
+    public static void mutate(double[] children){}
+
+    public static double[] newGen(double[] generation, double[] evaluation){
+        double[] parents = select(generation, evaluation);
+        double[] children = crossOver(parents);
+        children = mutate(children);
     }
 
     public static void main(String[] args){
         double[] generation = {2.3, 4.1, 1.8, 3.9, .9, 1.2, 4.8, 2.5, 3.2, .5};
         double[] evaluation = evaluate(generation);
+
+        for (int i = 0; i < 2; i++) {
+            generation = newGen(generation, evaluation);
+            evaluation = evaluate(generation);
+        }
     }
 }
